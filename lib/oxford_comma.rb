cd3b84dce761c0b9b14aee78175cebdb7891efae
Adam Fudge <coolfudge@gmail.com>
2017-09-25 19:42:43 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,15 @@
 def oxford_comma(array)
   return_string = ""
-
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return_string << array[0]
+    return_string << " and "
+    return_string << array[1]
+  else
+    
+  end
+  ", "
 
   return return_string
 end
